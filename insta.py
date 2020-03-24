90882adb6229f55c4f7361b20d32a705dd6b123d
unknown <vvilq27@gmail.com>
2020-03-24 21:57:03 +0100

    follow upgrade

--- insta.py
+++ insta.py
@@ -6,189 +6,32 @@ from util import getFollowing
 from util import getNonfollowers
 from util import unfollowUsers
 from util import likePosts
+from util import follow
+from util import likeHashtagPosts
 import sys
 
 from logger_util import get_logger
 
-from random import randint as r
 from datetime import date
 from time import sleep
 import os
-import re
+from subprocess import Popen
 
 excludes = ["dziecko", "hotel", "hotelspa", "spa", "zabiegi", "butik", "fashionblogger", 
 "mama", "nails", "studio", "kosmetyki", "krem", "fryzjer", "wellhair", "moda", "paznokcie", "salon", "sklep", 
 "mum", "maluch", "420", "memes", "memy", "clinic", "klinika", "polskichlopak", "brwi", "brows", "suchar", "fajne"]
 
+like_code = 'sqdOP yWX7d     _8A5w5   ZIAjV '
 exit_code = 'wpO6b '
+button_exit_class = '                   Igw0E     IwRSH      eGOV_         _4EzTm                                                                                  BI4qX            qJPeX            fm1AK   TxciK yiMZG'
 
-# TODO GENERAL
-# move all js scripts to constants file for easier updates
-# manual fix when stuck
-
-
-# TODO
-# add scrolling
-# add random post picking
-# add check for already liked today
-# add function final result
-# add comments if some comments already posted
-def likeHashtagPosts(amount, startOffset):
-	try: 
-		for postNum in range(amount):
-			browser.execute_script("document.getElementsByClassName(\"KL4Bh\")[{}].click()".format(postNum + startOffset))
-			sleep(r(1,100)/100.0 + r(2,4))
-			
-			
-			# check post hashtags
-			postValid = checkPostData()
-
-			print("Post valid: {}".format(postValid))
-
-			if not postValid:
-				# post invalid, close user window
-				browser.execute_script("document.getElementsByClassName(\"ckWGn\")[0].click()")
-				print("Post is invalid: ")
-				sleep(r(1,100)/10.0 + 1)
-				return
+# open chrome command
+# chrome.exe -remote-debugging-port=9014 --user-data-dir="C:\Users\aro\Documents\ChromeProfile" --headless
 
 
-			browser.execute_script("document.getElementsByClassName('dCJp8 afkep')[0].click()")
-			sleep(r(1,7) + 1)
-
-			# get username from post and save it in file
-			# TODO add timestamp
-			username = browser.execute_script("return document.getElementsByClassName('FPmhX notranslate  nJAzx')[0].href")
-			with open("likedImage.txt", "a+") as users:
-				users.write("%s;%s\n" % (str(date.today()), str(username)))
-
-			users.close()
-
-			# close user window
-			browser.execute_script("document.getElementsByClassName(\"ckWGn\")[0].click()")	
-			sleep(r(1,100)/100.0 + 1)
-
-
-	except NoSuchElementException as exception:
-		print(exception)
-		browser.refresh()
-
-	except JavascriptException as exception:
-		# user deleted image, close empty window
-		browser.execute_script("document.getElementsByClassName(\"ckWGn\")[0].click()")
-
-def follow(amount):
-	follow_counter = 0
-	post = 0
-
-	while follow_counter < amount:
-		post += 1
-		try: 
-			browser.execute_script("document.getElementsByClassName('KL4Bh')[{}].click()".format(post + 9 + 3 * 9))
-			sleep(r(1,100)/100.0 + r(2,4))
-
-			buttons = browser.execute_script("return document.getElementsByClassName('{}')".format(exit_code))
-			print(len(buttons))
-			exit_button = buttons[-1]
-			print(type(exit_button))
-			
-			# check post hashtags
-			postValid = checkPostData()
-
-			username = browser.execute_script("return document.getElementsByClassName('FPmhX notranslate  nJAzx')[0].href")
-
-			print("{}. post valid: {} for user: {}".format(follow_counter, postValid, username))
-
-			if postValid:
-				# try to follow, if user already followed go next post
-				try:
-					print('post valid, follow n like....')
-					browser.execute_script("document.getElementsByClassName('oW_lN sqdOP yWX7d    y3zKF     ')[0].click()")
-					sleep(r(1,3) + 1)
-					# like image
-					browser.execute_script("document.getElementsByClassName('wpO6b ')[0].click()")
-				except JavascriptException:
-					print("already following! like and close image")
-					browser.execute_script("document.getElementsByClassName('wpO6b ')[0].click()")
-					
-					sleep(r(1,7) + 1)
-					# browser.execute_script("document.getElementsByClassName(\"ckWGn\")[0].click()")
-					
-					browser.execute_script("arguments[0].click()", exit_button)
-					
-					continue
-
-				follow_counter += 1
-
-				sleep(r(1,100)/10.0 + 1)
-
-				# get username from post and save it in file
-				with open("ifollow.txt", "a+") as users:
-					users.write("%s;%s\n" % (str(date.today()), str(username)))
-
-				users.close()
-
-				# close user window
-				sleep(r(1,2) + 1)
-				# browser.execute_script("document.getElementsByClassName('{}')[0].click()".format(exit_code))
-				exit_button.click()
-				sleep(r(1,2) + 1)
-				try:
-					# browser.execute_script("document.getElementsByClassName('{}')[0].click()".format(exit_code))
-					exit_button.click()
-					print('HAD TO DOUBLE EXIT')
-				except JavascriptException:
-					print("[insta.py - follow] cant close window")
-
-				sleep(r(1,100)/100.0 + 1)
-
-				# scroll page
-				if follow_counter % 3 == 0:
-					browser.execute_script("window.scrollBy(0,350)")
-
-			else:
-				# post invalid, close user window
-				try:
-					print('click exit....')
-					# browser.execute_script("document.getElementsByClassName('{}')[0].click()".format(exit_code))
-					exit_button.click()
-				except:
-					print("cant close invalid post")
-				print("Post is invalid: ")
-				sleep(r(1,100)/10.0 + 1)
-
-				continue
-
-		except NoSuchElementException as exception:
-			print(exception)
-			browser.refresh()
-
-		except JavascriptException as exception:
-			# user deleted image, close empty window
-			# browser.execute_script("document.getElementsByClassName('wpO6b ')[0].click()")
-			exit_button.click()
-
-# TODO
-# return invalidity reason/hashtag
-# fix it
-# add try catch
-def checkPostData():
-	postValid = True
-	strPost = browser.execute_script("return document.querySelector(\"body > div._2dDPU.vCf6V > div.zZYga > div > article > div.eo2As > div.EtaWk > ul > div > li > div > div > div.C4VMK > span\").innerText")
-
-	listPostWords = strPost.split(' ')
-
-	for postWord in listPostWords:
-		for badWord in excludes:
-			if re.search(badWord, postWord):
-				print("found bad word: {}".format(postWord))
-				postValid = False
-
-	return postValid
-
-# os.system('cmd /c "chrome.exe -remote-debugging-port=9014 --user-data-dir=\"C:/Users/aro/Documents/ChromeProfile\""')
-# sleep(4)
-# print("go")
+# def init():
+# command = 'chrome.exe -remote-debugging-port=9014 --user-data-dir="C:/Users/aro/Documents/ChromeProfile"'
+# proc=Popen(command)
 
 chrome_options = Options()
 chrome_options.add_experimental_option("debuggerAddress", "localhost:9014")
@@ -197,24 +40,33 @@ chrome_options.add_argument('headless')
 chrome_driver = "C:/Users/aro/Documents/chromedriver/chromedriver.exe"
 browser = webdriver.Chrome(chrome_driver, chrome_options=chrome_options)
 
+# browser.get("https://www.instagram.com/")
+
+# close popup
+# prob need to send keys "enter" to get that
+# browser.find_elements_by_class_name('aOOlW   HoLwm ')[0].click()
+# browser.find_element_by_xpath('//div[4]/div/div/div[3]/button[2]').click()
+
 log = get_logger()
-likePosts(browser, log, int(sys.argv[1]))
+
+# browser.execute_script("window.scrollBy(0, {})".format(1000))
+
+# likePosts(browser, log, int(sys.argv[1]))
+# likeHashtagPosts(browser, log, 45, 35)
+
+# like = browser.find_elements_by_class_name(like_code)[0]
+# print(like.getTagName())
+# like.click()
 
 # likeHashtagPosts(43, 18)
+# browser.find_elements_by_class_name(like_code)[0].click()
 
-# follow(35)
-# sleep(0.5)
-# browser.execute_script("window.scrollBy(0,150)")
+follow(browser, log, 5)
 
-# browser.execute_script('document.getElementsByTagName('article')[1].getElementsByClassName('glyphsSpriteHeart__outline__24__grey_9 u-__7')[1].click()')
-
-
-# follow(30)
-	
 
 # getFollowers(browser)
 # getFollowing(browser)
 
 # getNonfollowers()
 
-# unfollowUsers(browser)
+# unfollowUsers(browser, log)
--- util.py
+++ util.py
@@ -5,11 +5,21 @@ import random
 import csv
 import re
 from datetime import date, datetime
+from random import randint as r
+import re
 
 excludes = ["dziecko", "hotel", "hotelspa", "spa", "zabiegi", "butik", "fashionblogger", 
 "mama", "nails", "studio", "kosmetyki", "krem", "fryzjer", "wellhair", "moda", "paznokcie", "salon", "sklep", 
 "mum", "maluch", "420", "memes", "mem", "memy", "clinic", "klinika", "polskichlopak", "brwi", "brows", "suchar",
-"brzuszku", "brzuszek", "ciąża", "official"]
+"brzuszku", "brzuszek", "ciąża", "official", "lash", "estetyczna", "mom", "dogsgram", "mother", "baby", "child", "przedłużanie"]
+
+invalid_names = ["polk", "polsk", "polish", "polan", "official", "makeup", "meme"]
+
+min_like_count = 20
+max_like_count = 225
+
+like_code = 'wpO6b '
+user_name_code = 'sqdOP yWX7d     _8A5w5   ZIAjV '
 
 
 def getFollowers(browser):
@@ -130,7 +140,7 @@ def getNonfollowers():
 	for user in followers:
 		print(user)
 
-def unfollowUsers(browser):
+def unfollowUsers(browser, logger):
 	baseUrl = "https://www.instagram.com/"
 	removeUserList = []
 	removedUserList = []
@@ -139,9 +149,7 @@ def unfollowUsers(browser):
 	unfollow_button_class_name = '_5f5mN    -fzfL     _6VtSN     yZn4P   '
 	unfollow_button_class_name2 = 'BY3EC  sqdOP  L3NKy    _8A5w5    '
 
-	# browser.get((baseUrl + "arasssu/"))
-	# currentFollowedCount = browser.execute_script("return document.getElementsByClassName('g47SY ')[2].textContent")
-	# print("Current follow count: {}".format(currentFollowedCount))
+	SLEEP_TIME = 30
 
 	removeIndex = 1
 	removeCount = 0
@@ -152,18 +160,11 @@ def unfollowUsers(browser):
 
 	removeUsersFile.close()
 
-	for userName in removeUserList:
-		print(userName)
-		
-		lastUser = True if removeUserList.index(userName) == len(removeUserList) - 1 else False
-
-		print(( str(removeUserList.index(userName)) + ' ' + str(len(removeUserList)) + ' ' + str(lastUser) ))
-
+	for userName in removeUserList:	
 		if userName is "":
 			print("username is blank")
 			continue
 
-		print("{}. removing user: {}".format(removeIndex, userName))
 		userLink = (baseUrl + userName)
 		browser.get(userLink)
 		removeIndex += 1
@@ -181,7 +182,6 @@ def unfollowUsers(browser):
 					print("Can't click, user prob already unfollowed, continue")
 					continue
 
-			sleep(random.randint(2,5))
 			browser.execute_script("document.getElementsByClassName('aOOlW -Cab_   ')[0].click()")
 
 			# check if removing is going good
@@ -213,24 +213,20 @@ def unfollowUsers(browser):
 
 			removeUsersFile.close()
 
-			# if its last element then skip waiting
-			if lastUser:
-				sleepTime = max(5, random.gauss(20,15))
-				print("Sleeping for: {}".format(sleepTime))
-				sleep(sleepTime)
+			logger.info("{}. removed user: {}".format(removeIndex, userName))
+			sleep(SLEEP_TIME + abs(random.gauss(20,5)))
 
 		except NoSuchElementException as exception:
 			print('NoSuchElementException')
 
-	print("Session ended, removed: {}, could not remove {} users".format(len(removedUserList), len(removeUserList) - len(removedUserList)))
+	logger.info("Session ended, removed: {}, could not remove {} users".format(len(removedUserList), len(removeUserList) - len(removedUserList)))
 
 
 def likePosts(browser, logger, amount):
-	listUserNames = []
-	scrollHeight = 150
+	scrollHeight = 100
 	int_post_liked = 0
-	# TODO
-	# change for loop to while until amount is liked
+	scrollTime = 0.4
+	
 
 	userName = ''
 	prevUserName = ''
@@ -241,52 +237,56 @@ def likePosts(browser, logger, amount):
 		while userName == prevUserName:
 			browser.execute_script("window.scrollBy(0, {})".format(scrollHeight))
 
-			# article = browser.execute_script("return document.getElementsByTagName('article')[4]")
 			article = browser.execute_script("return document.getElementsByTagName('article')[4]")
-			
-			userName = article.find_element_by_xpath('.//header/div[2]/div[1]/div/h2/a').text
 
-			sleep(0.15)
+			try:
+				# userName = article.find_element_by_xpath('.//header/div[2]/div[1]/div/h2/a').text
+				userName = article.find_element_by_xpath('.//header/div[2]/div[1]/div/a').text
+			except NoSuchElementException:
+				# the post is a hastag
+				userName = article.find_element_by_xpath('.//header/div[2]/div[2]/div[1]/a').text
 
-		logger.info('{}. {}'.format(int_post_liked, userName))
+			sleep(scrollTime)
+
 		prevUserName = userName
 
 		try:
 			postText = article.find_element_by_xpath('.//div[2]/div[1]/div/div/span/span[1]').text
 		except NoSuchElementException:
+			print('[Warning] Cant find postText')
 			postValid = True
 
 		if postValid == None:
-			postValid = checkFollowedUserPost(postText)
-
-		print(("Is post valid? " +  str(postValid)))
+			postValid = checkFollowedUserPost(postText) 
 
 		if not postValid:
-			print("invalid, next post")
+			print("[invalid post text], next post")
 			continue
 
-		flagLikePost = checkRecentLikeDate(userName)
-		print(flagLikePost)
+		flagRecentlyLikedPost = checkRecentLikeDate(userName) # last like was..
+		flagPostLikeCount, likeCount = checkPostLikeCount(article, userName)
 
-		if userName not in listUserNames and flagLikePost:
+		if  flagRecentlyLikedPost and flagPostLikeCount:
 			try:
-				print('liking image...')
-				# buttonLike = article.find_elements_by_tag_name('button')[0]
-
-				buttonLike = article.find_elements_by_class_name('wpO6b')[0]
+				buttonLike = article.find_elements_by_class_name(like_code)[0]
 
 				# send keys is workaround for click() function not working here for some reason
 				buttonLike.send_keys("\n")
 				int_post_liked +=  1
+
+				logger.info('liking image: {}. {}'.format(int_post_liked, userName))
 				
 				sleep(random.randint(100,300)/100.0)
 			except NoSuchElementException:
 				print("Post already liked, user: {}".format(userName))
 		else:
-			print('User  {}  already liked in this session'.format(userName))
-			continue
+			if not flagRecentlyLikedPost:
+				print("User was liked within last 3 days, next post")
 
-		listUserNames.append(userName)
+			elif not flagPostLikeCount:
+				print('Post has too many likes ({}) or cant get likes count'.format(likeCount))
+
+			continue
 		
 		likeCounter(userName)
 
@@ -375,15 +375,258 @@ def checkRecentLikeDate(username):
 			today = date.today()
 			day_difference = (today - datetimeLastLikeDate.date()).days
 
-			print(('last like was: ' + str(day_difference)))	
+			print(('checkRecentLikeDate() | last like was: ' + str(day_difference)))	
 
 			break
 
 	if day_difference is not None and day_difference < min_day_difference:
 		flagLikePost = False
-		print("User was liked within last 3 days, next post")
 
 	return flagLikePost
 
 
 
+def checkPostLikeCount(article, userName):
+	result = False
+	like_count = 0
+
+	try:
+		# if len(article.find_elements_by_class_name('sqdOP')) == 1:
+		# 	print('[INFO] checkPostLikeCount() | post is a video or a hashtag, next')
+		# 	return False, 0
+
+		like_count = article.find_elements_by_class_name('sqdOP')[1]
+		
+		# pares like txt for case when sbdy liked post
+		if like_count.text == '' or like_count == 'Opublikuj':
+			# like_count = article.find_elements_by_class_name('sqdOP')[1].find_element_by_xpath('.//span').text
+			str_like_count = article.find_elements_by_class_name('sqdOP ')[2].text.replace(' ', '')
+
+			cut_index = 0
+			for c in str_like_count:
+				if c.isdigit():
+					cut_index += 1
+
+			like_count = str_like_count[:cut_index]
+
+			print("checkPostLikeCount() | {}".format(like_count))
+		else:
+			like_count = like_count.text
+		result = True
+	except NoSuchElementException:
+		try:
+			like_count = article.find_element_by_xpath('//div[2]/section[2]/div/div/button/span').text
+			# like_count = article.find_elements_by_class_name('sqdOP')[9].find_elements_by_tag
+			result = True
+		except:
+			print("[INFO] checkPostLikeCount() | cant get likes from xpath")
+			result = False
+	
+
+	if result == True:
+		try:
+			like_count = str(like_count).replace(' ', '').split(':')[1]
+			like_count = int(like_count)
+			
+			if like_count < min_like_count or like_count > max_like_count:
+				result = False
+		except:
+			result = False
+			print("[INFO] ({}) something wrong with xpath: '{}'".format(userName, like_count))
+	else:
+		print("[Error] cant get like count")
+
+	return result, like_count
+
+
+def follow(browser, logger, amount):
+	SLEEP_TIME = 30
+	SCROLL_HEIGHT = 370
+	follow_counter = 0
+	post = 0
+
+	post_code = 'eLAPa'
+	username_code = 'sqdOP '
+
+	buttons = None
+	userName = None
+
+	while follow_counter < amount:
+		# scroll page
+		if post % 3 == 0:
+			scroll(browser, SCROLL_HEIGHT)
+
+		post += 1
+
+		try: 
+			article = browser.execute_script("return document.getElementsByClassName('{}')[{}].click()".format(post_code, (post % 3) + 35))
+
+			try:
+				userName = browser.execute_script("return document.getElementsByClassName('{}')[1].href".format(username_code))
+			except JavascriptException:
+				print("chuja a nie username")
+				
+			userNameValid = validUsername(userName)
+
+			sleep(r(1,100)/100.0 + r(1,2))
+			
+			# check post likeHashtagPosts
+			postValid = checkPostData(browser, False)
+			
+			if postValid and userNameValid:
+				# try to follow, if user already followed go next post
+				try:
+					browser.execute_script("document.getElementsByClassName('oW_lN sqdOP yWX7d    y3zKF     ')[0].click()")
+					sleep(r(1,3) + 1)
+					# like image
+					browser.execute_script("document.getElementsByClassName('wpO6b ')[0].click()")
+				except JavascriptException:
+					print("already following! like and close image")
+					browser.execute_script("document.getElementsByClassName('wpO6b ')[0].click()")
+					
+					sleep(r(1,7) + 1)
+					exitPost(browser)
+					
+					continue
+
+				logger.info("{}. followed user: {}".format(follow_counter, userName))
+				follow_counter += 1
+				sleep(r(30,50))
+
+				# get username from post and save it in file
+				with open("ifollow.txt", "a+") as users:
+					users.write("%s;%s\n" % (str(date.today()), str(userName)))
+
+				users.close()
+
+				# close user window
+				sleep(r(1,2) + 1)
+				
+				exitPost(browser)
+				# sleep(r(1,2) + 1)
+				sleep(1)
+
+
+			else:
+				# post invalid, close user window
+				print("post invalid for user: {}, username valid? {}".format(userName, userNameValid))
+					
+				exitPost(browser)
+
+				sleep(r(1,100)/10.0 + 1)
+
+				continue
+
+		except NoSuchElementException as exception:
+			print("[ERROR] NoSuchElementException: \r\n{}".format(exception))
+			browser.refresh()
+
+		except JavascriptException as exception:
+			# user deleted image, close empty window
+			print("[ERROR] JS exception: \r\n{}".format(exception) )
+			sleep(1.5)
+			exitPost(browser)
+
+		sleep(3)
+
+
+def checkPostData(browser, flagPrint = True):
+	postValid = True
+	strPostDescription = None
+
+	try:
+		strPostDescription  = browser.find_element_by_xpath('.//div[2]/div[1]/ul/div/li/div/div/div[2]/span').text
+	except NoSuchElementException:
+		try:
+			strPostDescription  = browser.find_element_by_xpath('.//div[2]/div[1]/ul/ul[1]/div/li/div/div[1]/div[2]/span').text
+		except Exception:
+			print("[Error] checkPostData | Cannot find post description")
+			postValid = False
+
+	if postValid is not False:
+		if flagPrint:
+			print(strPostDescription)
+		listPostWords = strPostDescription.split(' ')
+
+		for postWord in listPostWords:
+			for badWord in excludes:
+				if re.search(badWord, postWord):
+					print("checkPostData | found bad word: {}".format(postWord))
+					postValid = False
+					return False
+
+	return postValid
+
+
+def likeHashtagPosts(browser, log, like_amount, startOffset):
+	SCROLL_HEIGHT = 400
+	like_code = 'wpO6b '
+	username_code = 'sqdOP '
+	post_counter = 0
+	like_counter = 1
+
+	try: 
+		while like_counter < like_amount:
+			if post_counter % 3 == 0:
+				scroll(browser, SCROLL_HEIGHT)
+				print('>>>>>>>>>>>\t\tscrolll timeeeeeee: {}'.format(post_counter))
+				post_counter = 0
+
+			browser.execute_script("document.getElementsByClassName(\"KL4Bh\")[{}].click()".format(post_counter + startOffset+1 ))
+			sleep(r(1,100)/100.0 + r(2,4))
+			post_counter += 1
+			
+			# check post hashtags
+			postValid = checkPostData(browser)
+
+			if not postValid:
+				sleep(r(1,100)/10.0 + 1)
+
+				continue
+
+			browser.execute_script("document.getElementsByClassName('{}')[0].click()".format(like_code))
+			sleep(r(1,7) + 1)
+
+			# get username from post and save it in file
+			# TODO add timestamp
+			username = browser.execute_script("return document.getElementsByClassName('{}')[1].href".format(username_code))
+			print("{}. Post valid: {} for user: {}".format(like_counter, postValid, username))
+			
+			with open("likedImage.txt", "a+") as users:
+				users.write("%s;%s\n" % (str(date.today()), str(username)))
+
+			users.close()
+
+			print('>>>>>>>>> \t\t counter check: {}'.format(post_counter))
+			like_counter += 1
+
+
+			# close user window
+			exitPost(browser)
+			sleep(r(4,10))
+
+
+	except NoSuchElementException as exception:
+		print(exception)
+		browser.refresh()
+
+	except JavascriptException as exception:
+		# click blind spot to exit
+		exitPost(browser)
+		print('exit post')
+		print(exception)
+
+
+def exitPost(browser):
+	browser.execute_script('document.elementFromPoint(10, 10).click()')
+
+def scroll(browser, height = 350):
+	browser.execute_script("window.scrollBy(0,%s)" % height)
+
+def validUsername(username):
+
+	for badWord in invalid_names:
+			if re.search(badWord, username):
+				return False
+
+	return True
